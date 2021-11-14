de9373648a787ee57283e4b27159cad425c99afe
Baciejowski <k.baciejowski@gmail.com>
2021-11-14 23:02:30 +0100

    Added migration (new field: url in scenario)

---
+++ Backend/Migrations/20211114215304_AddedUrlInScenario.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backend.Migrations
+{
+    public partial class AddedUrlInScenario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Scenarios",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Scenarios");
+        }
+    }
+}
