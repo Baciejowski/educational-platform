2fbada35e9066f80e9544112fe365c2e1516c51b
swrczk <harleenincube@gmail.com>
2021-11-29 00:11:43 +0100

    install vue charts

--- ClientApp/src/components/Report/GeneralReport.vue
+++ ClientApp/src/components/Report/GeneralReport.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-      XYZ
-  </div>
+    <div>
+        XYZ
+        <D3PieChart :config="chart_config" :datum="chart_data"></D3PieChart>
+    </div>
 </template>
 
 <script>
-export default {
+import { D3PieChart } from "vue-d3-charts"
 
+export default {
+    components: {
+        D3PieChart
+    },
+    data() {
+        return {
+            chart_config: {
+                margin: { top: 100, bottom: 20 },
+                key: "name",
+                value: "total",
+                color: {
+                    scheme: ["#55D6BE", "#ACFCD9", "#7D5BA6", "#DDDDDD", "#FC6471"]
+                },
+                radius: { inner: 50 }
+            },
+            chart_data: [
+                {
+                    name: "Lorem",
+                    total: 30,
+                    color: "#0000FF"
+                },
+                {
+                    name: "Ipsum",
+                    total: 21,
+                    color: "#00FF00"
+                },
+                {
+                    name: "Dolor",
+                    total: 20,
+                    color: "#FF0000"
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style>
-
 </style>
