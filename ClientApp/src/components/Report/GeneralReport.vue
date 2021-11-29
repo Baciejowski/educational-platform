3a3ec937227b0d2dace76dc722610c615bd6a3f2
swrczk <harleenincube@gmail.com>
2021-11-29 03:12:58 +0100

    basic mocked charts

--- ClientApp/src/components/Report/GeneralReport.vue
+++ ClientApp/src/components/Report/GeneralReport.vue
@@ -1,44 +1,131 @@
 <template>
-    <div>
-        XYZ
-        <D3PieChart :config="chart_config" :datum="chart_data"></D3PieChart>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <D3PieChart :config="pie_config" :datum="participation_data" title="Participation"></D3PieChart>
+            </div>
+            <div class="col-6">
+                <D3PieChart :config="pie_config" :datum="passed_data" title="Scenario passed"></D3PieChart>
+            </div>
+        </div>
+
+        <D3BarChart :config="bar_config" :datum="skills_data" title="Skills %"></D3BarChart>
+
+        <D3BarChart :config="bar_config" :datum="time_data" title="Time per scenario"></D3BarChart>
+
+        <D3LineChart :config="line_config" :datum="difficultyDevelopment_data" title="Difficulty scalling factory"></D3LineChart>
+
+        <D3BarChart :config="bar_multiple_config" :datum="correctness_data" title="Question correctness"></D3BarChart>
     </div>
 </template>
 
 <script>
-import { D3PieChart } from "vue-d3-charts"
+import { D3PieChart, D3BarChart, D3LineChart } from "vue-d3-charts"
 
 export default {
     components: {
-        D3PieChart
+        D3PieChart,
+        D3BarChart,
+        D3LineChart
     },
     data() {
         return {
-            chart_config: {
-                margin: { top: 100, bottom: 20 },
+            pie_config: {
                 key: "name",
                 value: "total",
                 color: {
                     scheme: ["#55D6BE", "#ACFCD9", "#7D5BA6", "#DDDDDD", "#FC6471"]
-                },
-                radius: { inner: 50 }
-            },
-            chart_data: [
-                {
-                    name: "Lorem",
-                    total: 30,
-                    color: "#0000FF"
-                },
-                {
-                    name: "Ipsum",
-                    total: 21,
-                    color: "#00FF00"
-                },
-                {
-                    name: "Dolor",
-                    total: 20,
-                    color: "#FF0000"
                 }
+            },
+            bar_config: {
+                key: "name",
+                values: ["total"],
+
+                color: {
+                    scheme: ["#55D6BE", "#ACFCD9", "#7D5BA6", "#DDDDDD", "#FC6471"]
+                }
+            },
+            bar_multiple_config: {
+                key: "scenario",
+                values: ["random", "basic", "ai"],
+                color: {
+                    scheme: ["#55D6BE", "#ACFCD9", "#7D5BA6", "#DDDDDD", "#FC6471"]
+                }
+            },
+            line_config: {
+                values: ["aiDifficulty", "teacherDifficulty"],
+                date: {
+                    key: "date",
+                    inputFormat: "%Y",
+                    outputFormat: "%Y"
+                },
+                color: {
+                    scheme: ["#7D5BA6", "#FC6471"]
+                }
+            },
+            participation_data: [
+                {
+                    name: "Finished",
+                    total: 30
+                },
+                {
+                    name: "Pending",
+                    total: 21
+                }
+            ],
+            passed_data: [
+                {
+                    name: "Passed",
+                    total: 80
+                },
+                {
+                    name: "Failed",
+                    total: 21
+                }
+            ],
+            skills_data: [
+                {
+                    name: "Vision",
+                    total: 30
+                },
+                {
+                    name: "Light",
+                    total: 21
+                },
+                {
+                    name: "Speed",
+                    total: 60
+                }
+            ],
+            time_data: [
+                {
+                    name: "Part1",
+                    total: 80
+                },
+                {
+                    name: "Part2",
+                    total: 21
+                },
+                {
+                    name: "Intelligent",
+                    total: 60
+                },
+                {
+                    name: "Loneliness",
+                    total: 54
+                }
+            ],
+            difficultyDevelopment_data: [
+                { aiDifficulty: 1.2, teacherDifficulty: 1.0, date: 1 },
+                { aiDifficulty: 1.8, teacherDifficulty: 1.6, date: 2 },
+                { aiDifficulty: 2.1, teacherDifficulty: 1.5, date: 3 },
+                { aiDifficulty: 2.9, teacherDifficulty: 1.9, date: 4 }
+            ],
+            correctness_data: [
+                { random: 30, basic: 40, ai: 60, scenario: "Scenario 1" },
+                { random: 40, basic: 17, ai: 92, scenario: "Scenario 2" },
+                { random: 60, basic: 50, ai: 66, scenario: "Scenario 3" },
+                { random: 15, basic: 24, ai: 59, scenario: "Scenario 4" }
             ]
         }
     }
